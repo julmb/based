bf7505776354d5e2e076bfa2ae18a5cea47f0145
Julian Brunner <julian.brunner@gmail.com>
2025-06-20 05:09:05 +0200

    cleanup

--- source/Data/Based/Unfoldable.hs
+++ source/Data/Based/Unfoldable.hs
@@ -2,6 +2,7 @@ module Data.Based.Unfoldable (Unfoldable (..)) where
 
 import Data.Functor.Identity
 import Control.Applicative
+import Control.Monad
 import Control.Monad.Loops
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
@@ -9,7 +10,7 @@ import Data.Proxy
 import Data.List.NonEmpty (NonEmpty (..))
 
 invert :: Alternative f => Monad f => f a -> f ()
-invert a = optional a >>= maybe (pure ()) (const empty)
+invert = optional >=> maybe (pure ()) (const empty)
 
 class Unfoldable f where unfold :: Monad m => m (Maybe a) -> m (Maybe (f a))
 
