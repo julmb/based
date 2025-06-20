27b26e666e4d8c38e1e151fd92fce6bc462682ee
Julian Brunner <julian.brunner@gmail.com>
2025-06-20 05:08:05 +0200

    move invert

--- source/Data/Based/Tools.hs
+++ source/Data/Based/Tools.hs
@@ -1,6 +1,1 @@
-module Data.Based.Tools (invert) where
-
-import Control.Applicative
-
-invert :: Alternative f => Monad f => f a -> f ()
-invert a = optional a >>= maybe (pure mempty) (pure empty)
+module Data.Based.Tools () where
--- source/Data/Based/Unfoldable.hs
+++ source/Data/Based/Unfoldable.hs
@@ -1,12 +1,15 @@
 module Data.Based.Unfoldable (Unfoldable (..)) where
 
 import Data.Functor.Identity
+import Control.Applicative
 import Control.Monad.Loops
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 import Data.Proxy
 import Data.List.NonEmpty (NonEmpty (..))
-import Data.Based.Tools
+
+invert :: Alternative f => Monad f => f a -> f ()
+invert a = optional a >>= maybe (pure mempty) (pure empty)
 
 class Unfoldable f where unfold :: Monad m => m (Maybe a) -> m (Maybe (f a))
 
